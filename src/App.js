8d84f81bd09c570faf038ce58cbb60d01f1b476f
swapfile3 <swajeetg768@gmail.com>
2020-11-13 12:05:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Loading from './components/Loding';
+
+function App() {
+  return (
+    <>
+      <Loading />
+    </>
+  );
+}
+
+export default App;
